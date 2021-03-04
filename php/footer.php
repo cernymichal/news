3461e84ecedca7cb47ef46273c0a5faa1240ca6d
Michal Černý <cernyymichal@gmail.com>
2021-03-04 18:29:05 +0100

    show articles

--- php/footer.php
+++ php/footer.php
@@ -3,7 +3,7 @@
   <div class="container">
     <div class="row">
       <div class="col-lg-8 col-md-10 mx-auto">
-        <p class="copyright text-muted">Copyright &copy; Your Website 2020</p>
+        <p class="copyright text-muted">&copy; Články 2021</p>
       </div>
     </div>
   </div>
--- php/navigation.php
+++ php/navigation.php
@@ -1,7 +1,7 @@
 <!-- Navigation -->
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
   <div class="container">
-    <a class="navbar-brand" href="index.php">Start Bootstrap</a>
+    <a class="navbar-brand" href="index.php">Články</a>
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
       Menu
       <i class="fas fa-bars"></i>
@@ -9,7 +9,19 @@
     <div class="collapse navbar-collapse" id="navbarResponsive">
       <ul class="navbar-nav ml-auto">
         <li class="nav-item">
-          <a class="nav-link" href="index.php">Home</a>
+          <a class="nav-link" href="index.php">Zprávy</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="categories.php">Kategorie</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="authors.php">Autoři</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="administration.php">Administrace článků</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="add_article.php">Přidat článek</a>
         </li>
       </ul>
     </div>
